1c37efaa3f52a39c5d595077bec06743d58547b7
Seema Deshpande <seema.deshpande@synerzip.com>
2019-03-22 15:48:40 +0530

    Requested Changes done

--- src/browser/modules/EditorInfo/EditProperties.jsx
+++ src/browser/modules/EditorInfo/EditProperties.jsx
@@ -1,27 +1,39 @@
 import React from 'react'
 
 function EditProperties (props) {
+  const item = props.item.item._fields[0].properties
   return (
     <div>
-      {props.properties.map((item, index) => (
-        <p key={index}>
-          {' '}
-          {item.key} :{' '}
-          {props.disabled ? (
-            item.value
-          ) : (
-            <input
-              id='properties'
-              style={{ color: 'black' }}
-              type='text'
-              value={item.value}
-              onChange={e => {
-                props.handleChange(item.key, e)
-              }}
-            />
-          )}
-        </p>
-      ))}
+      {item &&
+        Object.keys(item).map(key => {
+          let value
+          if (typeof item[key] === 'object') {
+            value = item[key].high == 0 ? item[key].low : item[key].high
+          } else {
+            value = item[key]
+          }
+
+          return (
+            <div key={key}>
+              {key}:
+              {props.disabled ? (
+                value
+              ) : (
+                <div>
+                  <input
+                    id='item'
+                    style={{ color: 'black' }}
+                    type='text'
+                    value={item[key]}
+                    onChange={e => {
+                      props.handleChange(key, e)
+                    }}
+                  />
+                </div>
+              )}
+            </div>
+          )
+        })}
     </div>
   )
 }
