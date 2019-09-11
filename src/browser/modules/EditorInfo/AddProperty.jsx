0490dab5e9b21c2399dddeabaebedabf5e0b7a37
Shashank Gaikwad <shashank.gaikwad@synerzip.com>
2019-09-11 14:48:30 +0530

    Validations added for Add and Update properties

--- src/browser/modules/EditorInfo/AddProperty.jsx
+++ src/browser/modules/EditorInfo/AddProperty.jsx
@@ -214,19 +214,23 @@ function AddProperty (props) {
   }
 
   const onConfirmed = () => {
-    props.editEntityAction(
-      {
-        nodeId: props.nodeId,
-        key: p.key,
-        value: p.value,
-        oldProperties: Object.values(props.p)[0],
-        dataType: dataType
-      },
-      'update',
-      'nodeProperties'
-    )
-    setButtonVisibility(false)
-    dataType === 'date' && toggleCalendar(false)
+    if (p && p.key && p.value) {
+      props.editEntityAction(
+        {
+          nodeId: props.nodeId,
+          key: p.key,
+          value: p.value,
+          oldProperties: Object.values(props.p)[0],
+          dataType: dataType
+        },
+        'update',
+        'nodeProperties'
+      )
+      setButtonVisibility(false)
+      dataType === 'date' && toggleCalendar(false)
+    } else {
+      alert('Empty field')
+    }
   }
 
   const onCanceled = () => {
@@ -262,7 +266,7 @@ function AddProperty (props) {
             confirmIcon={<TickMarkIcon />}
             onConfirmed={() => {
               handleToggle(!textField)
-              if (p.key && p.value) {
+              if (p && p.key && p.value) {
                 props.editEntityAction(
                   {
                     id: props.id,
