85176e92f7ab719d81dbe04be2e26291992d4b03
Rahul Tarafdar <rahul.tarafdar@synerzip.com>
2019-09-18 02:00:01 +0530

    introducing reusable expansion panel

--- src/browser/modules/EditorInfo/ExpandRelationshipDetails.jsx
+++ src/browser/modules/EditorInfo/ExpandRelationshipDetails.jsx
@@ -1,113 +1,88 @@
-import React, { useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { PropertiesSection } from './DisplayNodeDetails'
-import {
-  DrawerSubHeader,
-  DrawerSectionBody
-} from 'browser-components/drawer/index'
+import { DrawerSubHeader } from 'browser-components/drawer/index'
 import { ConfirmationButton } from 'browser-components/buttons/ConfirmationButton'
+import { BinIcon } from 'browser-components/icons/Icons'
 import {
-  BinIcon,
-  ExpandMenuIcon,
-  CollapseMenuIcon
-} from 'browser-components/icons/Icons'
-import {
-  FoldersButton,
   StyledList,
   StyledListHeaderItem,
-  StyledFavFolderButtonSpan,
-  FolderButtonContainer,
-  StyledFolderLabel
+  StyledFavFolderButtonSpan
 } from '../Sidebar/styled'
 
-import classNames from 'classnames'
-import styles from '../DatabaseInfo/style_meta.css'
 import DisplayRelationshipType from './DisplayRelationshipType'
 import DisplayRelationshipDirection from './DisplayRelationshipDirection'
+import { ExpansionPanel } from './ExpansionPanel'
 
 /**
  * Component to Expand Relationship Details
  * @param {*} props
  */
 export const ExpandRelationshipDetails = props => {
-  const [active, setFlag] = useState(false)
+  let title = ''
+  if (props.relationshipEndpoint === 'from') {
+    title = `-- ${props.value.segments[0].relationship.type} --> 
+    ${Object.values(props.value.end.properties)[0] ||
+      props.value.end.identity.toInt()}`
+  } else if (props.relationshipEndpoint === 'to') {
+    title = `<--  ${
+      props.value.segments[0].relationship.type
+    }  -- ${Object.values(props.value.end.properties)[0] ||
+      props.value.end.identity.toInt()}`
+  }
+  const panelActions = (
+    <StyledFavFolderButtonSpan>
+      <ConfirmationButton
+        requestIcon={<BinIcon />}
+        confirmIcon={<BinIcon deleteAction />}
+        onConfirmed={() => {
+          // delete the relationship based on ID
+          props.editEntityAction(
+            {
+              relationshipId: props.value.segments[0].relationship.identity.toInt(),
+              nodeId: props.selectedNodeId
+            },
+            'delete',
+            'relationship'
+          )
+        }}
+      />
+    </StyledFavFolderButtonSpan>
+  )
 
   return (
     <div>
       <StyledList>
         <StyledListHeaderItem>
-          <StyledFolderLabel>
-            <DrawerSectionBody
-              className={classNames({
-                [styles['wrapper']]: true
-              })}
-            >
-              {props.relationshipEndpoint === 'from' &&
-                `-- ${props.value.segments[0].relationship.type} --> 
-                  ${Object.values(props.value.end.properties)[0] ||
-                    props.value.end.identity.toInt()}`}
-              {props.relationshipEndpoint === 'to' &&
-                `<-- 
-                  ${
-    props.value.segments[0].relationship.type
-    }  -- ${Object.values(props.value.end.properties)[0] ||
-                  props.value.end.identity.toInt()}`}
-            </DrawerSectionBody>
-          </StyledFolderLabel>
-          <FolderButtonContainer>
-            <FoldersButton onClick={() => setFlag(!active)}>
-              {active === true ? <CollapseMenuIcon /> : <ExpandMenuIcon />}
-            </FoldersButton>
-            &nbsp;
-            <StyledFavFolderButtonSpan>
-              <ConfirmationButton
-                requestIcon={<BinIcon />}
-                confirmIcon={<BinIcon deleteAction />}
-                onConfirmed={() => {
-                  // delete the relationship based on ID
-                  props.editEntityAction(
-                    {
-                      relationshipId: props.value.segments[0].relationship.identity.toInt(),
-                      nodeId: props.selectedNodeId
-                    },
-                    'delete',
-                    'relationship'
-                  )
-                }}
-              />
-            </StyledFavFolderButtonSpan>
-          </FolderButtonContainer>
-          {active === true && (
-            <div style={{ marginLeft: 30 }}>
-              <DrawerSubHeader>Relationship Type</DrawerSubHeader>
+          <ExpansionPanel title={title} panelActions={() => panelActions}>
+            <DrawerSubHeader>Relationship Type</DrawerSubHeader>
 
-              <DisplayRelationshipType
+            <DisplayRelationshipType
+              {...props}
+              relationshipType={props.value.segments[0].relationship.type}
+              relationshipId={props.value.segments[0].relationship.identity.toInt()}
+            />
+
+            <DrawerSubHeader>Relationship Direction</DrawerSubHeader>
+            <DisplayRelationshipDirection
+              {...props}
+              relationshipType={props.value.segments[0].relationship.type}
+              relationshipId={props.value.segments[0].relationship.identity.toInt()}
+            />
+
+            {props.value.segments.map((item, index) => (
+              <PropertiesSection
+                properties={
+                  item.relationship ? item.relationship.properties : null
+                }
                 {...props}
-                relationshipType={props.value.segments[0].relationship.type}
+                entityType='relationship'
+                editEntityAction={props.editEntityAction}
+                relationship={props.value.segments[0].relationship}
                 relationshipId={props.value.segments[0].relationship.identity.toInt()}
               />
-
-              <DrawerSubHeader>Relationship Direction</DrawerSubHeader>
-              <DisplayRelationshipDirection
-                {...props}
-                relationshipType={props.value.segments[0].relationship.type}
-                relationshipId={props.value.segments[0].relationship.identity.toInt()}
-              />
-
-              {props.value.segments.map((item, index) => (
-                <PropertiesSection
-                  properties={
-                    item.relationship ? item.relationship.properties : null
-                  }
-                  {...props}
-                  entityType='relationship'
-                  editEntityAction={props.editEntityAction}
-                  relationship={props.value.segments[0].relationship}
-                  relationshipId={props.value.segments[0].relationship.identity.toInt()}
-                />
-              ))}
-            </div>
-          )}
+            ))}
+          </ExpansionPanel>
         </StyledListHeaderItem>
       </StyledList>
     </div>
