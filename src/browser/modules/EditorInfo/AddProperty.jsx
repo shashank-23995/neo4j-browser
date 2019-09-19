3d7d757679d19f06038d826e1863a90d26b4c899
Rahul Tarafdar <rahul.tarafdar@synerzip.com>
2019-09-19 14:16:19 +0530

    ui changes for add property

--- src/browser/modules/EditorInfo/AddProperty.jsx
+++ src/browser/modules/EditorInfo/AddProperty.jsx
@@ -96,34 +96,40 @@ function AddProperty (props) {
   const [entityType, setEntityType] = useState('')
 
   // effect to copy props to state. this is one time job
-  useEffect(() => {
-    if (!stateUpdatedWithProps) {
-      setP(props.p)
-      const dataTypeValue = dataTypeChecker(
-        Object.values({ value: props.p && props.p.value })
-      )
-      setDatatype(dataTypeValue)
-      if (props.relationshipId !== null) {
-        setEntityType('relationship')
-      } else {
-        setEntityType('node')
+  useEffect(
+    () => {
+      if (!stateUpdatedWithProps) {
+        setP(props.p)
+        const dataTypeValue = dataTypeChecker(
+          Object.values({ value: props.p && props.p.value })
+        )
+        setDatatype(dataTypeValue)
+        if (props.relationshipId !== null) {
+          setEntityType('relationship')
+        } else {
+          setEntityType('node')
+        }
+        setFlag(true)
       }
-      setFlag(true)
-    }
-  }, [props])
+    },
+    [props]
+  )
 
   // effect to show confirmation buttons
-  useEffect(() => {
-    if (
-      stateUpdatedWithProps &&
-      props.p &&
-      (props.p.value !== p.value || props.p.key !== p.key)
-    ) {
-      setButtonVisibility(true)
-    } else {
-      setButtonVisibility(false)
-    }
-  }, [p && p.key, p && p.value, stateUpdatedWithProps])
+  useEffect(
+    () => {
+      if (
+        stateUpdatedWithProps &&
+        props.p &&
+        (props.p.value !== p.value || props.p.key !== p.key)
+      ) {
+        setButtonVisibility(true)
+      } else {
+        setButtonVisibility(false)
+      }
+    },
+    [p && p.key, p && p.value, stateUpdatedWithProps]
+  )
 
   const handleChange = (key1, value) => {
     setP({ ...p, value: value })
@@ -330,14 +336,19 @@ function AddProperty (props) {
       ) : null}
       {props.ToDisplay == 'view' || textField ? (
         <DrawerSection
-          style={{
-            marginLeft: -9,
-            marginRight: -9,
-            backgroundColor: '#efeff4',
-            borderBottomLeftRadius: 4,
-            borderBottomRightRadius: 4,
-            marginBottom: 0
-          }}
+          style={
+            textField
+              ? {
+                marginLeft: -9,
+                marginRight: -9,
+                backgroundColor: '#efeff4',
+                borderBottomLeftRadius: 4,
+                borderBottomRightRadius: 4,
+                marginBottom: 0,
+                padding: 5
+              }
+              : {}
+          }
         >
           {showButtons ? (
             <PartialConfirmationButtons
